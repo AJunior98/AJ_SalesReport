d7952d5401c4f365a46818712946acc12b20e001
Amauri Junior <apojr98@gmail.com>
2022-07-17 09:52:11 -0300

    Project created

---
+++ backend/src/main/java/com/ajunior/ajsalesreport/AjsalesreportApplication.java
@@ -0,0 +1,13 @@
+package com.ajunior.ajsalesreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjsalesreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjsalesreportApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ajunior/ajsalesreport/AjsalesreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajunior.ajsalesreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjsalesreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
